43f555f1fb22096b94b9e7d7399b9907ae0637a1
vishalsingh384 <vishalsing384@gmail.com>
2022-06-19 00:00:38 +0530

    :construction: gallery and db initiated

--- script.js
+++ script.js
@@ -12,24 +12,24 @@ let constraints = {
     audio:true
 }
 
-let shouldRecord = false;
-
-navigator.mediaDevices.getUserMedia(constraints)
+navigator.mediaDevices.getUserMedia(constraints)//The Navigator.mediaDevices read-only property returns a "MediaDevices" object, 
+//which provides access to connected media input devices like cameras and microphones, as well as screen sharing.
 .then((stream) => {
     video.srcObject = stream;
 
-    recorder = new MediaRecorder(stream);
+    recorder = new MediaRecorder(stream);//Creates a new MediaRecorder object, given a MediaStream to record. MediaStream() is a constructor.
     recorder.addEventListener("start", (e) => {
         //memory
         chunks = [];
         console.log('rec started');
-    })
+    });
 
+    //The dataavailable event is fired when the MediaRecorder delivers media data to your application for its use. The data is provided in a Blob object that contains the data
     recorder.addEventListener("dataavailable", (e) => {
         chunks.push(e.data);
         console.log('recording pushed in chunks');
     });
-
+    
     recorder.addEventListener("stop", () => {
         //convert video 
         let blob = new Blob(chunks, { type: 'video/mp4' });
@@ -38,12 +38,12 @@ navigator.mediaDevices.getUserMedia(constraints)
         let videoURL = URL.createObjectURL(blob);
         console.log(videoURL);
         
-        let a = document.createElement('a');
-        a.href = videoURL;
-        a.download = "myVideo.mp4";
-        a.click();
+        // let a = document.createElement('a');
+        // a.href = videoURL;
+        // a.download = "myVideo.mp4";
+        // a.click();
         //store in database
-    })
+    });
 
 
 });
@@ -54,7 +54,7 @@ captureBtnCont.addEventListener("click", () => {
     let canvas = document.createElement("canvas");
 
     let tool = canvas.getContext("2d");
-    canvas.width = video.videoWidth;
+    canvas.width = video.videoWidth;//Gets the intrinsic width of a video in CSS pixels, or zero if the dimensions are not known.
     canvas.height = video.videoHeight;
 
     tool.drawImage(video, 0, 0, canvas.width, canvas.height);
@@ -73,21 +73,27 @@ captureBtnCont.addEventListener("click", () => {
 
 });
 
-recordBtnCont.addEventListener("click", () => {
-    
+//start recording 
+let shouldRecord = false;
+recordBtnCont.addEventListener("click", () => { 
     shouldRecord = !shouldRecord;
     if (shouldRecord) {
         recordBtn.classList.add("scale-record");
+
         //recording start
-        recorder.start();
+        recorder.start();//Begins recording media; this method can optionally be passed a timeslice argument with a value in milliseconds. 
+        //If this is specified, the media will be captured in separate chunks of that duration, rather than the default behavior of recording the media in a single large chunk.
+        
         //start timer
         startTimer();
     }
 
     else {
         recordBtn.classList.remove("scale-record");
+
         //stop the recording
-        recorder.stop();
+        recorder.stop();//Stops recording, at which point a dataavailable event containing the final Blob of saved data is fired. No more recording occurs.
+        
         // stop the timer
         stopTimer();
     }
@@ -96,7 +102,7 @@ recordBtnCont.addEventListener("click", () => {
 
 let timer = document.querySelector('.timer');
 
-let counter = 0;
+let counter = 3590;
 let timerID;
 function startTimer() {
     timer.style.display = 'block';
@@ -105,24 +111,25 @@ function startTimer() {
 
         let hours = Number.parseInt(totalSeconds / 3600);
         totalSeconds = totalSeconds % 3600;
+        console.log(`${hours} hours and ${totalSeconds} totalSeconds`)
 
         let minutes = Number.parseInt(totalSeconds / 60);
         totalSeconds = totalSeconds % 60;
+        console.log(`${minutes} minutes and ${totalSeconds} totalSeconds`)
+
 
         let seconds = totalSeconds;
+        console.log(`${seconds} seconds`)
 
-        hours = (hours < 10) ? `0 ${hours}` : hours;
-        minutes = (minutes < 10) ? `0 ${minutes}` : minutes;
-        seconds = (seconds < 10) ? `0 ${seconds}` : seconds;
+        hours = (hours < 10) ? `0${hours}` : hours;
+        minutes = (minutes < 10) ? `0${minutes}` : minutes;
+        seconds = (seconds < 10) ? `0${seconds}` : seconds;
         timer.innerText = `${hours}:${minutes}:${seconds}`;
 
         counter++;
-
     }
-
     timerID = setInterval(displayTimer, 1000);
-    counter = 0;
-
+    counter = 3650;
 }
 
 function stopTimer() {
@@ -140,6 +147,10 @@ let allFilters = document.querySelectorAll(".filter");
 allFilters.forEach((filterElem) => {
     filterElem.addEventListener('click', () => {
         transparentColor = getComputedStyle(filterElem).getPropertyValue('background-color');
+        //The Window.getComputedStyle() method returns an object containing the values of all CSS properties of an element
+
+        //The CSSStyleDeclaration.getPropertyValue() method interface returns a string containing the value of a specified CSS property.
+        
         filterLayer.style.backgroundColor = transparentColor;
     })
 })
